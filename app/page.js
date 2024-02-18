0c5bdf0c05a35bad03f96ae8985a172ffd9bfa23
lavranjansingh <niddusingh4@gmail.com>
2024-02-18 14:46:09 +0530

    ehjgdfjk

--- app/page.js
+++ app/page.js
@@ -1,7 +1,6 @@
 import BannerAreaHome from "@/Components/BannerAreaHome";
 import Header from "@/Components/Header";
 // import WhyChooseUs from "@/Components/WhyChooseUs";
-import Image from "next/image";
 
 export default function Home() {
   return (
