8ffe1b350c8804a420a6782c92cbda48611ae656
Deepak Gathwal <rickygathwal@gmail.com>
2024-02-20 14:58:10 +0530

    changes

--- utils/redisFile.js
+++ utils/redisFile.js
@@ -3,19 +3,3 @@ import { createClient } from 'redis';
 export const client = await createClient()
   .on('error', err => console.log('Redis Client Error', err))
   .connect();
-
-
-
-
-
-
-//   DB_HOST=localhost
-// DB_USER=root
-// DB_PASS=''
-// DB_DATABASE=jtcindia_admin
-
-// EMAIL_HOST=smtp.elasticemail.com
-// EMAIL_PORT=2525
-// EMAIL_USER=rickykumargathwal@gmail.com
-// EMAIL_PASS='611AF2486A8DFDD2A24653D06E73DEEFA0B5'
-// EMAIL_TO=rickygathwal@gmail.com
