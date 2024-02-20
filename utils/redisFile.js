b94fd6a37972414221974a70e75336fd08f9bda3
Deepak Gathwal <rickygathwal@gmail.com>
2024-02-20 14:56:32 +0530

    changes

--- utils/redisFile.js
+++ utils/redisFile.js
@@ -7,3 +7,15 @@ export const client = await createClient()
 
 
 
+
+
+//   DB_HOST=localhost
+// DB_USER=root
+// DB_PASS=''
+// DB_DATABASE=jtcindia_admin
+
+// EMAIL_HOST=smtp.elasticemail.com
+// EMAIL_PORT=2525
+// EMAIL_USER=rickykumargathwal@gmail.com
+// EMAIL_PASS='611AF2486A8DFDD2A24653D06E73DEEFA0B5'
+// EMAIL_TO=rickygathwal@gmail.com
