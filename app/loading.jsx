181aa4759f7f39945c0bb05228cf18c4de683d52
sharma171 <68746747+sharma171@users.noreply.github.com>
2024-03-17 15:51:29 +0530

    loader-public-shape

--- app/loading.jsx
+++ app/loading.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import Loader from './loader-gif.gif';
+import Loader from '../public/assets/images/shapes/loader-gif.gif';
 import Image from 'next/image';
 
 const Loading = () => {
   return (
     <div>
-      <div className='route-loader'><Image src={Loader} alt='loader' /></div>
+      <div className='route-loader'><Image src={Loader} alt='loader' width={500} /></div>
     </div>
   )
 }
